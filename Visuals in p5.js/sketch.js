756b3c89b549719eaff6bc7e721d4174c32a9d9b
KroSheChKa <104899233+KroSheChKa@users.noreply.github.com>
2025-03-01 17:20:51 +0300

    Update sketch.js

--- Visuals in p5.js/sketch.js
+++ Visuals in p5.js/sketch.js
@@ -1,5 +1,5 @@
-var rows = 100;
-var cols = 100;
+var rows = 200;
+var cols = 200;
 var grid = [];
 
 var openSet = [];
@@ -11,9 +11,17 @@ var nosolution = false;
 
 var circles = [];
 
+var fps_counter = 0;
+
+var canvas_width = 750;
+var canvas_height = 750;
+
 function heuristic(a, b) {
-  var d = dist(a.i, a.j, b.i, b.j)
-  // var d = abs(a.i - b.i) + abs(a.j, b.j)
+  
+  var d = Math.sqrt((a.i - b.i)*(a.i - b.i) + (a.j - b.j)*(a.j - b.j));
+  // var d = max(abs(a.i - b.i), abs(a.j - b.j))
+  // var d = dist(a.i, a.j, b.i, b.j)
+  // var d = abs(a.i - b.i) + abs(a.j - b.j)
   return d
 }
 
@@ -36,61 +44,55 @@ function Spot(i, j, circles) {
   this.wall = false;
   this.circles = circles;
   
-  // var if_1 = random(1) < 0.5;
+  this.inOpenSet = false;
+  this.inClosedSet = false;
+
   var if_1 = false;
   
   for (let p = 0; p < circles.length; p++) {
     x = circles[p][0];
     y = circles[p][1];
     r = circles[p][2];
-    if_1 = pow(i - x, 2) + pow(j - y, 2) < r*r;
-    if (if_1 == true) {
+    if_1 = (i - x)*(i - x) + (j - y)*(j - y) < r*r;
+    if (if_1) {
+      this.wall = true;
       break;
     }
   }
   
-  if (if_1 & !(i == 0 & j == 0) & !(i == cols - 1 & j == rows-1)){
-    this.wall = true;
-  }
-  
-  this.show = function(col) {
-    fill(col);
-    if (this.wall == true) {
-      fill(0)
+  this.show = function(col, layer = undefined) {
+    if (layer == bgLayer) {
+      bgLayer.stroke(0);
+      if (this.wall == true) {
+        bgLayer.fill(0);
+      } else {
+        bgLayer.fill(col);
+      }
+      bgLayer.rect(this.i*w, this.j*h, w, h);
+    } else {
+      stroke(0);
+      fill(col);
+      rect(this.i*w, this.j*h, w, h);
     }
-    rect(this.i*w, this.j*h, w, h);
   }
   
   this.addNeighbors = function(grid) {
+    // no left movement
     if (i < cols - 1) {
       this.neighbors.push(grid[i+1][j]);
     }
-    // no left movement
-    // if (i > 0) {
-    //   this.neighbors.push(grid[i-1][j]);
-    // }
     if (j < rows - 1) {
       this.neighbors.push(grid[i][j+1]);
     }
     if (j > 0) {
       this.neighbors.push(grid[i][j-1]);
     }
-    
     if (j < rows - 1 && i < cols - 1) {
       this.neighbors.push(grid[i+1][j+1]);
-    }
-    // no left movement
-    // if (j > 0 && i > 0) {
-    //   this.neighbors.push(grid[i-1][j-1]);
-    // }
-    
+    }  
     if (j > 0 && i < cols - 1) {
       this.neighbors.push(grid[i+1][j-1]);
     }
-    // no left movement
-    // if (j < rows - 1 && i > 0) {
-    //   this.neighbors.push(grid[i-1][j+1]);
-    // }
   }
 }
 
@@ -101,22 +103,28 @@ function generatCircles(count) {
     var y = random(rows*0.1, rows*0.9);
     circles.push([x, y, r])
   }
-  // return circles;
 }
 
 function setup() {
-  createCanvas(670, 670);
+  createCanvas(canvas_width, canvas_height);
+  bgLayer = createGraphics(canvas_width, canvas_height);
   
   w = width/cols;
   h = height/rows;
   
-  generatCircles(25);
+  strokeWeight(w / 4);
+  bgLayer.background(255)
+  bgLayer.noFill();
+  bgLayer.strokeWeight(w / 4);
+  bgLayer.stroke(255);
   
+  generatCircles(25);
   
   for (let i = 0; i < cols; i++) {
     grid[i] = [];
     for (let j = 0; j < rows; j++) {
       grid[i][j] = new Spot(i, j, circles);
+      grid[i][j].show(255, bgLayer)
     }
   }
   
@@ -129,11 +137,17 @@ function setup() {
   start = grid[0][0];
   end = grid[cols-1][rows-1];
   
+  end.show(color(255, 215, 0), bgLayer);
+  start.show(color(255, 215, 0), bgLayer);
+  
   openSet.push(start);
+  start.inClosedSet = true;
+  
+  start.wall = false;
+  end.wall = false;
 }
 
 function draw() {
-  background(0);
   
   if (openSet.length > 0) {
     // great we back on
@@ -144,11 +158,8 @@ function draw() {
         winner = i;
       }
     }
-    
 
-    var current = openSetHeap.extractMin();
-
-
+    var current = openSet[winner];
     
     //exit
     if (current === end) {
@@ -163,11 +174,11 @@ function draw() {
     for(var i = 0; i < neighbors.length; i++) {
       var neighbor = neighbors[i];
       
-      if (!closedSet.includes(neighbor) && !neighbor.wall) {
+      if (!neighbor.inClosedSet && !neighbor.wall) {
         var tempG = current.g + heuristic(neighbor, current);
 
         var newPath = false;
-        if (openSet.includes(neighbor)) {
+        if (neighbor.inOpenSet) {
           if (tempG < neighbor.g) {
             neighbor.g = tempG
             newPath = true;
@@ -175,12 +186,13 @@ function draw() {
         } else {
           neighbor.g = tempG;
           newPath = true;
+          neighbor.inOpenSet = true;
           openSet.push(neighbor);
         }
         
         if (newPath) {
-          neighbor.h = pow(heuristic(neighbor, end), 11);
-          neighbor.f = neighbor.g + neighbor.h;
+          neighbor.h = heuristic(neighbor, end);
+          neighbor.f = neighbor.g + neighbor.h*neighbor.h;
           neighbor.previous = current;
         }
       }
@@ -192,44 +204,43 @@ function draw() {
   }
   
   
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      grid[i][j].show(255);
+  image(bgLayer, 0, 0);
+  // drawing
+  if (fps_counter % 1 == 0) {
+    
+    for (let i = 0; i < closedSet.length; i++) {
+      closedSet[i].show(color(230, 0, 0))
     }
-  }
-  
-  for (let i = 0; i < closedSet.length; i++) {
-    closedSet[i].show(color(230, 0, 0))
-  }
-      
-  for (let i = 0; i < openSet.length; i++) {
-    openSet[i].show(color(0, 230, 0))
-  }
-  
-  if (!nosolution) {
-    path = [];
-    var temp = current;
-    while (temp.previous) {
-    path.push(temp.previous);
-    temp = temp.previous;
-    }
-  }
-  
 
-  for (let i = 0; i < path.length; i++) {
-    path[i].show(color(0, 0, 230));
-  }
+    for (let i = 0; i < openSet.length; i++) {
+      openSet[i].show(color(0, 230, 0))
+    }
+
+    if (!nosolution) {
+      path = [];
+      var temp = current;
+      while (temp.previous) {
+        path.push(temp.previous);
+        temp = temp.previous;
+      }
+    }
+
+
+    for (let i = 0; i < path.length; i++) {
+      path[i].show(color(0, 0, 230));
+    }
+
+    noFill();
+    stroke(255);
+    beginShape();
+    for (let i = 0; i < path.length; i++) {
+      vertex(path[i].i*w + w/2, path[i].j*h + h/2);
+    }
+    endShape();
+    stroke(0);
+
+    }
   
-  noFill();
-  strokeWeight(w / 4);
-  stroke(255);
-  beginShape();
-  for (let i = 0; i < path.length; i++) {
-    vertex(path[i].i*w + w/2, path[i].j*h + h/2);
-  }
-  endShape();
-  stroke(0);
+  fps_counter +=1;
   
-  end.show(color(255, 215, 0));
-  start.show(color(255, 215, 0));
 }
